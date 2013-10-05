0a914b87f143782d5c927740acc9de251ec7a2fc
Andrew Dunham <andrew@du.nham.ca>
2013-10-04 21:03:57 -0700

    Make tests trivially runnable without py.test

---
+++ specter/tests/__main__.py
@@ -0,0 +1,20 @@
+import unittest
+
+# Import test modules.
+from .test_events import *
+from .test_forms import *
+from .test_frames import *
+from .test_navigation import *
+from .test_open import *
+from .test_qtmessage import *
+from .test_redirection import *
+from .test_registry import *
+from .test_selectors import *
+from .test_signals import *
+from .test_simple import *
+from .test_ssl import *
+from .test_util import *
+
+
+if __name__ == "__main__":
+    unittest.main()
